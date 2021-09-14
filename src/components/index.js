fd7e682cf52510df1e5f33df35bc6cf13bab3186
rafaelpbar <rafael.coper@outlook.com>
2021-09-13 21:07:15 -0300

    Iniciando o projeto

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as ImageCard } from './ImageCard';
+export { default as ImageSkeleton } from './ImageSkeleton';
+export { default as Loader } from './Loader';
+export { default as Map } from './Map';
+export { default as Modal } from './Modal';
+export { default as RestaurantCard } from './RestaurantCard';
+export { default as Text } from './Text';
